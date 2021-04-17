e2ad8c88a6de45f3741b64757eac6af67044bae7
Kevin Wright <kw.stat@gmail.com>
2021-04-17 03:50:02 +0000

    version 1.2

---
+++ NEWS.md
@@ -0,0 +1,16 @@
+# pagenum 1.2 - Apr 2021
+
+Add rmarkdown to Suggests
+
+
+# pagenum 1.1 - Jul 2017
+
+* Now using `testthat` and `covr` packages.
+
+# pagenum 1.0 - Dec 2015
+
+* First release to CRAN.
+
+# pagenum 0.0 - 2006
+
+* Development begins.  See: tolstoy.newcastle.edu.au/R/help/06/06/30031.html
--- inst/doc/pagenum_examples.R
+++ inst/doc/pagenum_examples.R
@@ -1,14 +1,14 @@
-## ----setup, message=FALSE------------------------------------------------
+## ----setup, message=FALSE-----------------------------------------------------
 library("knitr")
 knitr::opts_chunk$set(fig.align="center", fig.width=6, fig.height=6)
 library("pagenum")
 
-## ----base----------------------------------------------------------------
+## ----base---------------------------------------------------------------------
 setPagenum(1)
 plot(Sepal.Length~Sepal.Width, data=iris, col=Species, pch=19)
 pagenum()
 
-## ----multi---------------------------------------------------------------
+## ----multi--------------------------------------------------------------------
 op = par(mfrow=c(1,2))
 plot(Sepal.Length ~ Sepal.Width, data=iris, col=Species, pch=19)
 plot(Petal.Length ~ Petal.Width, data=iris, col=Species, pch=19)
@@ -18,19 +18,16 @@ pagenum(num="", text="Draft",
         x=.5, y=.95, just=c('center','top'),
         col="wheat", cex=3)
 
-## ----lattice-------------------------------------------------------------
+## ----lattice------------------------------------------------------------------
 setPagenum(getPagenum()+1) # Manual increment
 library("lattice")
 xyplot(Sepal.Length~Sepal.Width, data=iris, groups=Species)
 pagenum(date=TRUE)
 
-## ----ggplot--------------------------------------------------------------
+## ----ggplot-------------------------------------------------------------------
 library("ggplot2")
 ggplot(iris, aes(x=Sepal.Width, y=Sepal.Length,
                  color=Species)) + geom_point() + theme_classic()
 pagenum(text="ABC Corp - ", date=TRUE,
         x=.95, y=.95, just=c('right','top'))
-
-## ----session-------------------------------------------------------------
-sessionInfo()
 
